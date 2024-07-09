5b10b586376c661350eed294f3d7769194e89580
Lima Araújo <158223826+limaraujo@users.noreply.github.com>
2024-07-09 20:39:17 -0300

    Update t.cpp

--- t.cpp
+++ t.cpp
@@ -1,72 +1,91 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+#define INF 1000000000
+#define UNVISITED 0
+#define VISITED 1
+
+struct element {
+    int parent, distance;
+    element(int p, int d) : parent(p), distance(d) {}
+};
+
 class Graph {
 private:
-    int numNode;
-    vector<vector<int>> adjList;
-    vector<int> inDegree;
-    vector<int> topoOrder;
+    unordered_map<int, vector<pair<int, int>>> adjList;
+    unordered_map<int, int> mark;
+    int numEdges, numNodes;
 
 public:
-    Graph(int n) : numNode(n), adjList(n + 1), inDegree(n + 1, 0) {}
-
-    void addEdge(int u, int v) {
-        adjList[u].push_back(v);
-        inDegree[v]++;
+    Graph(int N) : numEdges(0), numNodes(N) {
+        for (int i = 0; i < N; i++) {
+            adjList[i];
+            mark[i] = UNVISITED;
+        }
     }
 
-    bool topologicalSort() {
-        priority_queue<int, vector<int>, greater<int>> pq;
-
-        for (int i = 1; i <= numNode; ++i) {
-            if (inDegree[i] == 0) {
-                pq.push(i);
-            }
-        }
-
-        while (!pq.empty()) {
-            int u = pq.top();
-            pq.pop();
-            topoOrder.push_back(u);
-
-            for (int v : adjList[u]) {
-                if (--inDegree[v] == 0) {
-                    pq.push(v);
-                }
-            }
-        }
-
-        return topoOrder.size() == numNode;
+    void setEdge(int i, int j, int wt) {
+        adjList[i].emplace_back(j, wt);
+        numEdges++;
     }
 
-    void printTopoOrder() {
-        if (!topologicalSort()) {
-            cout << "Sandro fails." << endl;
-        } else {
-            for (int i = 0; i < numNode; ++i) {
-                cout << topoOrder[i];
-                if (i < numNode - 1) {
-                    cout << " ";
+    void dijkstra(int s, int D[]) {
+        int P[numNodes]; // P array for storing predecessors
+        priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>, greater<pair<int, pair<int, int>>>> H;
+        int p, v;
+
+        for (int i = 0; i < numNodes; i++) {
+            D[i] = INF;
+            mark[i] = UNVISITED;
+            P[i] = -1; // Initialize P array
+        }
+
+        H.push({0, {s, s}});
+        D[s] = 0;
+
+        for (int i = 0; i < numNodes; i++) {
+            do {
+                if (H.empty()) return;
+                auto top = H.top(); H.pop();
+                p = top.second.first;
+                v = top.second.second;
+            } while (mark[v] == VISITED);
+
+            if (v == -1) return;
+            mark[v] = VISITED;
+            P[v] = p;
+
+            for (auto& neighbor : adjList[v]) {
+                int w = neighbor.first;
+                int weight = neighbor.second;
+                if (mark[w] != VISITED && D[w] > D[v] + weight) {
+                    D[w] = D[v] + weight;
+                    H.push({D[w], {v, w}});
                 }
             }
-            cout << endl;
         }
     }
 };
 
 int main() {
-    int n, m;
-    cin >> n >> m;
-    Graph G(n);
+    int N = 5; // Número de nós
+    Graph g(N);
 
-    for (int i = 0; i < m; ++i) {
-        int x, y;
-        cin >> x >> y;
-        G.addEdge(x, y);
+    g.setEdge(0, 1, 10);
+    g.setEdge(0, 2, 3);
+    g.setEdge(0, 3, 20);
+    g.setEdge(1, 3, 5);
+    g.setEdge(2, 1, 2);
+    g.setEdge(2, 4, 15);
+    g.setEdge(3, 4, 11);
+    
+    int D[N];
+    g.dijkstra(0, D);
+
+    cout << "Distâncias mínimas a partir do nó 0:" << endl;
+    for (int i = 0; i < N; i++) {
+        cout << "Distância até o nó " << i << ": " << D[i] << endl;
     }
-
-    G.printTopoOrder();
 
     return 0;
 }
